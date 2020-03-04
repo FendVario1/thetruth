888f0f9a13e1d0dc31cfda9582659dfdf64a6e81
Andreas Hellmich <andreas.hellmich@fau.de>
2020-03-04 11:59:57 +0100

    Added BufferManagerTest Stub

---
+++ src/test/java/eu/rationality/thetruth/BufferManagerTest.java
@@ -0,0 +1,24 @@
+package eu.rationality.thetruth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BufferManagerTest {
+
+    @Test
+    public void getinstance() {
+    }
+
+    @Test
+    public void byid() {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void deregister() {
+    }
+}
