251bab23640690f4171f54deb031174972f2845a
Andreas Hellmich <andreas.hellmich@fau.de>
2020-03-09 14:09:57 +0100

    Rudimentary c support for 2nd user
    This is currently causing major issues and therefore atm not further implemented

--- native/plugin/config.h
+++ native/plugin/config.h
@@ -1,7 +1,9 @@
 
 void the_truth_config_init(void);
-int the_truth_config_update_pass(const char *);
-int the_truth_config_update_jid(const char *);
+int the_truth_config_update_pass1(const char *);
+int the_truth_config_update_jid1(const char *);
+int the_truth_config_update_pass2(const char *);
+int the_truth_config_update_jid2(const char *);
 
 int the_truth_config_user_write_callback (const void *, void *, struct t_config_file *, const char *);
 
